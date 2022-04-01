43d1bb4fe5ebedde469752454687d8335ffcabea
Preston Darrough <techdarrough@gmail.com>
2022-03-31 21:03:37 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  let [counter, changeCounter] = useState(5);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>Hello</div>
+      <h1>{counter}</h1>
+      <button onClick={() => changeCounter(counter + 1)}>Plus</button>
+      <button onClick={() => changeCounter(counter - 1)}>Minus</button>
     </div>
   );
 }
